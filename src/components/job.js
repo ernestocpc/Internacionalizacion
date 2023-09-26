b4e25c3ff07a8661d414b3a0a384c845ac56b25a
Ernesto P <ec.perez@uniandes.edu.co>
2023-09-26 09:14:16 -0500

    act en clase

--- src/components/job.js
+++ src/components/job.js
@@ -1,14 +1,41 @@
 import React from "react";
+import { FormattedDate, FormattedMessage, FormattedNumber, FormattedPlural } from 'react-intl';
+import { isSpanish } from "../index";
 
 const Job = (props) => {
   return (
-    <tr>
+    <tr >
       <th scope="row">{props.offer.id}</th>
       <td>{props.offer.name}</td>
       <td>{props.offer.company}</td>
-      <td>{props.offer.salary}</td>
+      <td>
+        <span>
+          <FormattedNumber
+            value={props.offer.salary}
+            style="currency"
+            currency="COP"
+            minimumFractionDigits={0}
+          />{' '}
+          <FormattedPlural value={props.offer.salary} one={<FormattedMessage id="million"/>} other={<FormattedMessage id="millions"/>}/>
+        </span>
+
+
+      </td>
       <td>{props.offer.city}</td>
-      <td>{props.offer.date}</td>
+      <td>
+        <FormattedDate
+          value={new Date(props.offer.date)}
+          year='numeric'
+          month='long'
+          day='numeric'
+          weekday='long'
+        />
+      </td>
+      <td>
+        <FormattedNumber
+          value={props.offer.views}
+        />
+      </td>
     </tr>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { IntlProvider } from 'react-intl';
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 import JobsList from "./components/jobslist";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+const userLanguage = navigator.language || navigator.userLanguage;
+const isSpanish = userLanguage.startsWith('es');
+
+// Define the locale and messages based on the detected language
+const locale = isSpanish ? 'es-ES' : 'en-US';
+const messages = isSpanish ? localeEsMessages : localeEnMessages;
+
+ReactDOM.render(
+    <IntlProvider locale={locale} messages={messages}>
+        <JobsList />
+    </IntlProvider>, document.getElementById("root")
+);
+export { isSpanish };
